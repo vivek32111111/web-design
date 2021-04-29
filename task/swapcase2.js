d96d230b1d5e912c0648d7b72646d2bf7e9327ca
vk3796565@gmail.com <vk3796565@gmail.com>
2021-04-29 13:38:43 +0530

    swapcase solution 2

---
+++ task/swapcase2.js
@@ -0,0 +1,18 @@
+// Q7. Write a JavaScript function that takes a string which has lower and upper case letters as a parameter and converts upper case letters to lower case, and lower case letters to upper case?
+// File name suggestion: swapCase.js
+// Test Data :
+// console.log(swapCase(‘AaBbc’)); // “aAbBC”
+
+str ='AaBbc';
+res = '';
+for (var i = 0; i < str.length; ++i) {
+    c = str[i];
+  if (c == c.toUpperCase()) {
+    res += c.toLowerCase();
+  } else if (c == c.toLowerCase()) {
+    res += c.toUpperCase();
+  } else {
+    res += c;
+  }
+}
+console.log(res);
